95e622db0fad4a8028abd51c8c0289c0bac8df91
shriram ganesan <shriram.ganesan@gmail.com>
2018-10-28 00:17:29 +0530

    Initial Commit

---
+++ HomeMgmt/src/main/java/com/ram/homemgmt/HomeMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.ram.homemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMgmtApplication.class, args);
+	}
+}
